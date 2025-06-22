8d56d87fd5e046095a7810d1b50dc71f9047719c
LƯU ĐỨC DŨNG <157196568+DungCoderk5@users.noreply.github.com>
2025-06-22 23:38:03 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());  // Đọc cookie từ request
+
+app.use(cors());
 
 module.exports = app;
