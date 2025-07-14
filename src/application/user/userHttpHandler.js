ebf41b2d7afdfd5777e18af2ae80148355a76864
tantruc69 <tantrucfpt03@gmail.com>
2025-07-14 10:19:05 +0700

    Update userHttpHandler.js

--- src/application/user/userHttpHandler.js
+++ src/application/user/userHttpHandler.js
@@ -130,7 +130,7 @@ async function changePasswordHandler(req, res) {
   try {
     // const userId = req.body?.user_id; // hoặc lấy từ JWT decode
     const {userId, oldPassword, newPassword } = req.body;
-
+console.log('[DEBUG] req.body:', req.body);
     if (!oldPassword || !newPassword) {
       return res.status(400).json({ error: 'Thiếu mật khẩu cũ hoặc mới' });
     }
