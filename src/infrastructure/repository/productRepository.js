54a3e08a0650b6684e78aed992d1226a6428d53b
girist-00 <hoangntps41442@gmail.com>
2025-07-25 13:32:18 +0700

    Update productRepository.js

--- src/infrastructure/repository/productRepository.js
+++ src/infrastructure/repository/productRepository.js
@@ -664,6 +664,10 @@ const productRepository = {
         gte: minPrice,
         lte: maxPrice,
       },
+      sale_price: {
+        gte: minPrice,
+        lte: maxPrice,
+      },
       ...(keyword?.trim()
         ? {
             name: {
