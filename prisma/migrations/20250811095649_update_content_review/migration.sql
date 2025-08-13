e0076441ccefcd66222ee36eaef6b87e10963099
LƯU ĐỨC DŨNG <157196568+DungCoderk5@users.noreply.github.com>
2025-08-13 13:52:23 +0700

    up

--- prisma/migrations/20250811095649_update_content_review/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `content` on table `product_reviews` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `product_reviews` MODIFY `content` TEXT NOT NULL;
