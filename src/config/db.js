f1ec3ba7ac79ea05b8015f501c9f261f87c4aa3a
baolcfrontend <luuducdungdcm@gmail.com>
2025-08-25 15:37:36 +0700

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -2,21 +2,16 @@ const mysql = require('mysql2/promise');
 
 const connectDB = async () => {
   try {
-    const connection = await mysql.createPool({
-      host: process.env.DB_HOST || "shortline.proxy.rlwy.net",
-      port: process.env.DB_PORT || 42289,
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "vUdUIjtsWyGVvgjCQwGBYqpzyZBlQqUF",
-      database: process.env.DB_NAME || "railway",
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     });
-
-    console.log('✅ MySQL connected');
+    console.log('MySQL connected');
     return connection;
   } catch (error) {
-    console.error(' MySQL connection failed:', error.message);
+    console.error('MySQL connection failed:', error);
     process.exit(1);
   }
 };
