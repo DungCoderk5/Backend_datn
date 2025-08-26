cd8e0db820395802637048ecc89f7763897838f7
LƯU ĐỨC DŨNG <157196568+DungCoderk5@users.noreply.github.com>
2025-08-26 20:36:50 +0700

    sửa api

--- src/application/user/userHttpHandler.js
+++ src/application/user/userHttpHandler.js
@@ -29,7 +29,7 @@ const updateOrderStatusUsecase = require("../../infrastructure/usecase/user/upda
 const addUserVoucherUsecase = require("../../infrastructure/usecase/user/addUserVoucherUsecase");
 const getAllUsersUsecase = require("../../infrastructure/usecase/user/getAllUserUsecase");
 const updateUsersUsecase = require("../../infrastructure/usecase/user/updateUsersUsecase");
-const FRONTEND_URL = process.env.FRONTEND_URL
+const FRONTEND_URL = process.env.FRONTEND_URL;
 // Tạo repository và usecase
 const googleAuthRepository = new GoogleAuthRepository();
 const googleAuthUsecase = new GoogleAuthUsecase(googleAuthRepository);
@@ -75,7 +75,7 @@ async function googleCallback(req, res) {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // chỉ HTTPS khi production
       sameSite: "strict", // hoặc "strict"
-      maxAge: 3600 * 1000, // 1 giờ
+      maxAge: 60 * 60 * 24, // 24 giờ
     });
 
     res.json(result);
